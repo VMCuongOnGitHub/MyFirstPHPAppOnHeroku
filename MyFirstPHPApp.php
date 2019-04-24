10dc7bd5b33c0d01117c4cce1eaee976820bd34a
Vũ Mạnh Cường <vmc529440@gmail.com>
2019-04-24 13:56:15 +0700

    My second commit

--- MyFirstPHPApp.php
+++ MyFirstPHPApp.php
@@ -1,8 +1,11 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?>
- </body>
-</html>
+<php?
+    <html>
+     <head>
+      <title>PHP Test</title>
+     </head>
+     <body>
+     <?php echo '<p>Hello World</p>'; ?>
+     </body>
+    </html>  
+?>
+
