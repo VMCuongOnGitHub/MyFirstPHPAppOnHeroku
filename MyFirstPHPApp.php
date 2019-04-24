fae14b9ad30ac544f48dc3fceaa5e5a2910056ba
Vũ Mạnh Cường <vmc529440@gmail.com>
2019-04-24 13:58:04 +0700

    Trying to deploy

--- MyFirstPHPApp.php
+++ MyFirstPHPApp.php
@@ -1,11 +1,10 @@
-<php?
-    <html>
+<html>
      <head>
       <title>PHP Test</title>
      </head>
      <body>
      <?php echo '<p>Hello World</p>'; ?>
      </body>
-    </html>  
-?>
+    </html>
+     
 
