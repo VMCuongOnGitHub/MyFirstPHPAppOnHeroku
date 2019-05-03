380c308160b8082d2f1d102c5cc0367354d774d9
Vu Manh Cuong <vmc529440@gmail.com>
2019-05-03 14:04:00 +0700

    sd

--- connect-admin.php
+++ connect-admin.php
@@ -4,7 +4,7 @@ $user = sanitizeString($_POST['username']);
 $pass = sanitizeString($_POST['password']);
 
 
-$sql = "SELECT * FROM Users WHERE username = '$user' AND password = '$pass'";
+$sql = "SELECT * FROM users WHERE username = '$user' AND password = '$pass'";
 $db = parse_url(getenv("DATABASE_URL"));
 $pdo = new PDO("pgsql:" . sprintf(
     "db_host=%s;db_port=%s;db_user=%s;db_password=%s;db_database_name=%s",
