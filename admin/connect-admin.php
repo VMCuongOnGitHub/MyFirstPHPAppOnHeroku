6f95a36d8fbf050db7b1e2fca7a0e122e2edd0d0
Vũ Mạnh Cường <vmc529440@gmail.com>
2019-05-03 12:08:48 +0700

    test db2

--- admin/connect-admin.php
+++ admin/connect-admin.php
@@ -2,11 +2,11 @@
 
 $db = parse_url(getenv("DATABASE_URL"));
 $pdo = new PDO("pgsql:" . sprintf(
-    "db_host=%s;port=%s;db_user=%s;db_password=%s;db_database_name=%s",
+    "db_host=%s;db_port=%s;db_user=%s;db_password=%s;db_database_name=%s",
     $db['db_host'],
     $db['db_user'],
     $db['db_password'],
-    $db['db_database_name'],
+    $db['db_port'],
     ltrim($db["path"], "/")
 ));
 
--- connect.php
+++ connect.php
@@ -1,12 +1,12 @@
 <?php
   $db = parse_url(getenv("DATABASE_URL"));
   $pdo = new PDO("pgsql:" . sprintf(
-      "db_host=%s;port=%s;db_user=%s;db_password=%s;db_database_name=%s",
-      $db['db_host'],
-      $db['db_user'],
-      $db['db_password'],
-      $db['db_database_name'],
-      ltrim($db["path"], "/")
+    "db_host=%s;db_port=%s;db_user=%s;db_password=%s;db_database_name=%s",
+    $db['db_host'],
+    $db['db_user'],
+    $db['db_password'],
+    $db['db_port'],
+    ltrim($db["path"], "/")
   ));
 
   // Convert all valuable in to UPPERCASE
